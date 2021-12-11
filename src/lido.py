32a684702f0ecbb04eb4ddc0865ddbffbed01d08
Maurice Mengel <mauricemengel@gmail.com>
2021-12-11 13:51:42 +0100

    lido.py: let's append to log file instead of starting a new one for every run

--- src/lido.py
+++ src/lido.py
@@ -84,8 +84,9 @@ class LidoTool:
             self.outdir.mkdir(parents=False, exist_ok=False)
         print(f" outdir {self.outdir}")
         logfile = self.outdir.joinpath("lidoTool.log")
+        #let's append to the log file so we can aggregrate results from multiple runs
         logging.basicConfig(
-            filename=logfile, filemode="w", encoding="utf-8", level=logging.INFO
+            filename=logfile, filemode="a", encoding="utf-8", level=logging.INFO
         )
 
     #
